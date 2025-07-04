e40272cf35ee46d8a0028eb0dd0d7ed7624d60bc
l-martinetti <luca.martinetti@outlook.it>
2025-07-04 16:16:30 +0200

    build: add basic project files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { GlobalProvider } from "./contexts/GlobalContext";
+
+import HomePage from "./pages/HomePage"
+
+function App() {
+
+  return (
+    <>
+    <GlobalProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={HomePage}/>
+        </Routes>
+      </BrowserRouter>
+    </GlobalProvider>
+    </>
+  )
+}
+
+export default App
