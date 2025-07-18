e6844e39e0a8fa67e514f656f953f23638da8a07
l-martinetti <luca.martinetti@outlook.it>
2025-07-18 14:46:07 +0200

    feat: add searchbar to videogames index

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,29 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useGlobalContext } from "../contexts/GlobalContext";
 import Card from "../components/card";
 
 const HomePage = () => {
     const {videogames, fetchVideogames} = useGlobalContext();
+    const [search, setSearch] = useState("");
 
     useEffect(() => fetchVideogames(), []);
 
-    return (
+    const filteredVideogames = videogames?.filter(videogame =>
+        videogame.title.toLowerCase().includes(search.toLowerCase())
+    );
+
+     return (
         <div className="container">
-            <h1 className="p-2 text-white fs-3">Latest News</h1>
-            <div className="container">
-                <div className="row row-cols-1">
-                {
-                    videogames?.map(videogame => (
+            <input
+                type="text"
+                className="form-control my-4 w-50"
+                placeholder="Cerca un videogioco..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <h1 className="p-2 text-white fs-3">Latest Games</h1>
+
+            <div className="row row-cols-1">
+                {filteredVideogames?.length > 0 ? (
+                    filteredVideogames.map(videogame => (
                         <div key={videogame.id} className="col mb-5">
-                            <Card videogame={videogame}/> 
+                            <Card videogame={videogame} />
                         </div>
-                        
                     ))
-                }
-                </div>
+                ) : (
+                    <p className="text-white">Nessun risultato trovato.</p>
+                )}
             </div>
         </div>
-    )
+    );
 }
 
 export default HomePage
